7a1802d23354f49ca38db74ec7a888b177511d62
Milena <milenadjj1998@gmail.com>
2019-11-27 00:09:37 +0100

    Osigurava da je dom ucitan pre pokusaja dodavanja header-a

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,16 @@
 function main(){
 
-    $.get("nav.html", function(data){
-        $("#nav_bar").replaceWith(data);
-        
-        const pageId = this.location.pathname.split(".html")[0].substr(1);
-        const selectedPageButton = document.getElementById(pageId);
-        if(selectedPageButton){
-            selectedPageButton.classList.add("selectedPage");
-        }
-
-    }.bind(this));//this se odnosi na Window
+    window.addEventListener('DOMContentLoaded', (event) => {
+        $.get("nav.html", function(data){
+            $("#nav_bar").replaceWith(data);
+            
+            const pageId = this.location.pathname.split(".html")[0].substr(1);
+            const selectedPageButton = document.getElementById(pageId);
+            if(selectedPageButton){
+                selectedPageButton.classList.add("selectedPage");
+            }
+    
+        }.bind(this));//this se odnosi na Window
+    });
 }
 main();
